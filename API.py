5462b1232adfc111f4cab4671353375311674736
ArchaicGamer <dylansmith02@gmail.com>
2021-08-12 16:27:49 -0700

    Update API.py

--- API.py
+++ API.py
@@ -7,6 +7,11 @@ from PIL import Image
 app = Flask(__name__)
 
 
+UPLOAD_FOLDER = '/nfs/stak/users/smithdyl/CS361/CS-361/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/', methods=['POST'])
 def resize():
     print(request.files)
