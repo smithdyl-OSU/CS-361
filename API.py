5860ecce50b2fe3dd8b39e719968c40dae0a48a9
ArchaicGamer <dylansmith02@gmail.com>
2021-08-12 16:19:00 -0700

    Fixed bug with API.py

--- API.py
+++ API.py
@@ -1,4 +1,4 @@
-import img_transformer
+from img_transformer import image_resize
 from flask import request
 from flask import Flask
 from flask import send_file
@@ -11,6 +11,6 @@ app = Flask(__name__)
 def resize():
     print(request.files)
     file = request.files['image']
-    img = Image.open(file.stream, )
+    img = Image.open(file.stream)
     resized_img = image_resize(img)
     return send_file(resized_img, mimetype='resized_image/jpg')
